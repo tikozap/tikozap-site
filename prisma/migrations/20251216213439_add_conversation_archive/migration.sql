53d33978fa0d315b9a117c313a1c09c024c3a773
Kevin Zhang <you@example.com>
2026-01-10 14:13:42 -0500

    Init Postgres migrations

--- prisma/migrations/20251216213439_add_conversation_archive/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Conversation" ADD COLUMN "archivedAt" DATETIME;
-
--- CreateIndex
-CREATE INDEX "Conversation_tenantId_archivedAt_idx" ON "Conversation"("tenantId", "archivedAt");
--- prisma/migrations/20260105174909_widget_settings/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Widget" ADD COLUMN "assistantName" TEXT;
-ALTER TABLE "Widget" ADD COLUMN "brandColor" TEXT;
-ALTER TABLE "Widget" ADD COLUMN "greeting" TEXT;
